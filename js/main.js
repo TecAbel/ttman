ea0594faa7ccc41fa3fa35947664e92567db2e83
TecAbel <abelardo96.work@gmail.com>
2019-12-01 19:33:35 -0600

    menu movil listo

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function () {
+    
+})();
+
+$(function(){
+    /**Menu responsive */
+    $(".menu-movil").click(function () { 
+        $(".navegacion-principal").slideToggle();
+        console.log('cliclk en menu');
+    });
+});
